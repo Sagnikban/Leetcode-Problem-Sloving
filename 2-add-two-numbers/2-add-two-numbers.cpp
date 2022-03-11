b5d0e24e81910e9e5cb327009d9615e837b7998c
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-03-11 21:26:34 +0530

    Time: 43 ms (66.79%), Space: 71.5 MB (50.11%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -7,76 +7,39 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
-**/
+ */
 class Solution {
 public:
-        string findSum(string str1, string str2){
-    
-    if (str1.length() > str2.length())
-        swap(str1, str2);
-    string str = "";
-    int n1 = str1.length(), n2 = str2.length();
-    reverse(str1.begin(), str1.end());
-    reverse(str2.begin(), str2.end());
- 
-    int carry = 0;
-    for (int i=0; i<n1; i++)
-    {
-        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
-    }
-    for (int i=n1; i<n2; i++)
-    {
-        int sum = ((str2[i]-'0')+carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
-    }
-    if (carry)
-        str.push_back(carry+'0');
-    reverse(str.begin(), str.end());
-    return str;
-}
-ListNode* convertStringToLL(string s)
-{
-   
-    if(s.size() == 0) return NULL;
-
-  ListNode* tmp=new ListNode(int(s[0])-48); 
-  ListNode* ans=tmp;  
-  
-  for(int k=1;k<s.size();k++)
-  {
-  tmp->next= new ListNode(int(s[k])-48);
-  tmp=tmp->next;
-  }
-return ans;
-}
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-            string list1,list2;
-            ListNode* temp = l1;
-            ListNode* temp1 = l2;
-            
-            while(temp!=NULL){
-                    string v = to_string(temp->val);
-                    list1.append(v);
-                    temp = temp->next;
+        ListNode* dummy= new ListNode();
+        ListNode* temp=dummy;
+        
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry)
+        {
+            int sum=0;
+            if(l1!=NULL)
+            {
+                 sum+=l1->val;
+                l1=l1->next;
             }
-            while(temp1!=NULL){
-                    string v = to_string(temp1->val);
-                    list2.append(v);
-                    temp1 = temp1->next;
+            
+             if(l2!=NULL)
+            {
+                 sum+=l2->val;
+                l2=l2->next;
             }
- 
-            reverse(list1.begin(),list1.end());
-           reverse(list2.begin(),list2.end());
-            string res = findSum(list1,list2);
-      
-            reverse(res.begin(),res.end());
             
-            return convertStringToLL(res);
+            sum+=carry;
+            carry=sum/10;
+            ListNode *node =new ListNode(sum%10);
+            temp->next=node;
+            temp=temp->next; 
             
-            
+        }
+        
+        return dummy->next;
+        
         
     }
 };
