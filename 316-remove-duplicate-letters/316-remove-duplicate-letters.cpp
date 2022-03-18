c425e2ad4c5204109d339d12641299aae1c820b1
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-03-18 12:15:35 +0530

    Time: 5 ms (37.14%), Space: 6.9 MB (20.77%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       
+        vector<bool> exist(26,false);
+        
+      
+        
+        stack<char>st;
+        unordered_map<char,int>m;
+        
+        for(int i=0;i<s.length();i++)
+        m[s[i]]++;
+        
+        for(int i=0;i<s.length();i++)
+        {
+                 m[s[i]]--;
+            
+                if(exist[s[i]-'a']==false)
+              
+                {
+                while(st.size()>0 && st.top()>s[i] && m[st.top()]>0)
+                {
+                     exist[st.top()-'a']=false;
+                     st.pop();
+                } 
+                st.push(s[i]);
+                exist[s[i]-'a']=true;
+                }
+        }   
+        
+        string ans="";
+        
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+          reverse(ans.begin(),ans.end());
+        return ans;
+            
+    }
+};
