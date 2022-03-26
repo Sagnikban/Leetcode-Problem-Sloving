714a9b3f86c13b2ef8418729f525e6044971f8ff
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-03-26 11:00:21 +0530

    Time: 154 ms (31.73%), Space: 47 MB (7.10%) - LeetHub

---
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,47 @@
+#define mod 1e9+7
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>&A) {
+        stack<long long>s1,s2;
+        vector<long long>next_smaller(A.size());
+        vector<long long>pre_smaller(A.size());
+        
+        for(long long i=0;i<A.size();i++)
+        {
+            next_smaller[i]=A.size()-1-i;
+            pre_smaller[i]=i;
+        }
+        
+        for(long long i=0;i<A.size();i++)
+        {
+            while(!s1.empty() && A[s1.top()]>A[i])
+            {
+                next_smaller[s1.top()]=i-s1.top()-1;
+                s1.pop();
+            }
+            
+            s1.push(i);
+        }
+        
+         for(long long i=A.size()-1;i>=0;i--)
+        {
+            while(!s2.empty() && A[s2.top()]>=A[i])
+            {
+                pre_smaller[s2.top()]=s2.top()-i-1;
+                s2.pop();
+            }
+            
+            s2.push(i);
+        }
+        
+        long long ans=0;
+        
+        for(long long i=0;i<A.size();i++)
+        {
+            ans+=(A[i]*(pre_smaller[i]+1)*(next_smaller[i]+1));
+            ans%=(long long)mod;
+        }
+        
+        return ans;
+    }
+};
