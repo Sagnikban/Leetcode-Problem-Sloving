6fcb594061621b67d0d64aa780c03442368f9ffd
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-04 10:28:14 +0530

    Time: 215 ms (39.14%), Space: 72.9 MB (26.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+       int count =0;
+        unordered_map<int,int> mp;
+        
+        for(int i =0;i<nums.size();i++){
+        
+            int diff = k - nums[i];  
+            if(mp[diff]>0){
+                
+                count++;               // increase count since we got the pair
+                mp[diff]--;            // decrease freq of diff as we used it for our pair 
+                
+            }
+            else{
+                mp[nums[i]]++;         // no pair found so put it in hashmap
+            }
+            
+            
+        }
+        
+        return count;
+        
+        
+       /* sort(nums.begin(),nums.end());
         int i=0,j=nums.size()-1;
         int ans=0;
         while(i<j)
@@ -22,6 +44,6 @@ public:
             }
         }
         
-        return ans;
+        return ans;*/
     }
 };
