a75bd8c30b31f10b19a0a15410ae96421183cbe0
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-04 10:52:38 +0530

    Added solution - LeetHub

---
+++ Hungry Pizza Lovers - GFG/hungry-pizza-lovers.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> permute(vector<vector<int>> &arr, int n);
+
+
+int main() {
+    
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            int a, b;
+            cin>> a>> b;
+            arr[i].push_back(a);
+            arr[i].push_back(b);
+        }
+        
+        vector<int> ans;
+        ans = permute(arr, n);
+        for(int i=0;i<n;i++)
+            cout << ans[i] << "\n";
+        
+    }
+    
+	return 0;
+}// } Driver Code Ends
+
+
+vector<int> permute(vector<vector<int>> &arr, int n)
+{
+    
+    // Complete the functmion
+    vector<pair<int,int>>v;
+    map<int,int>m;
+    for(int i=0;i<arr.size();i++)
+    {
+        v.push_back({arr[i][0]+arr[i][1],i+1});
+    }
+    
+    sort(v.begin(),v.end());
+    vector<int>ans;
+    for(auto x:v)
+    {
+        ans.push_back(x.second);
+    }
+    
+    return ans;
+    
+    
+}
