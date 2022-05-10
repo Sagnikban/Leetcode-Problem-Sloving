0cc31616a690bc228da92429c5eb0860f5da3645
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-10 09:15:00 +0530

    Time: 3 ms (45.41%), Space: 6.7 MB (37.26%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -12,17 +12,17 @@ public:
         if(k==0 || n==0 || sum==0)
         return;    
 
-        
-       
-           res.push_back(n);
-           recurse(k-1,n-1,res,sum-n,ans);
-           res.pop_back();
-           recurse(k,n-1,res,sum,ans) ;
+        res.push_back(n);
+        recurse(k-1,n-1,res,sum-n,ans);
+        res.pop_back();
+        recurse(k,n-1,res,sum,ans) ;
         
     }
+    
     vector<vector<int>> combinationSum3(int k, int n) {
+        
         vector<int>res;
-            vector<vector<int>>ans;
+        vector<vector<int>>ans;
         int sum=n;
         recurse(k,9,res,sum,ans);
         
