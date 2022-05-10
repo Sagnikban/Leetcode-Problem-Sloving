ba97ae76cce789db7a65417dab66a75b3d5bc1ba
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-10 09:14:05 +0530

    Time: 8 ms (9.10%), Space: 6.6 MB (42.22%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void recurse(int k,int n,vector<int>&res,int sum)
+
+    void recurse(int k,int n,vector<int>&res,int sum,vector<vector<int>>&ans)
     {
         if(k==0  && sum==0)
         {
@@ -13,18 +13,18 @@ public:
         return;    
 
         
-          if(find(res.begin(),res.end(),n)==res.end())
-          {
+       
            res.push_back(n);
-           recurse(k-1,n-1,res,sum-n);
+           recurse(k-1,n-1,res,sum-n,ans);
            res.pop_back();
-           recurse(k,n-1,res,sum) ;
-        }
+           recurse(k,n-1,res,sum,ans) ;
+        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>res;
+            vector<vector<int>>ans;
         int sum=n;
-        recurse(k,9,res,sum);
+        recurse(k,9,res,sum,ans);
         
         return ans;
     }
