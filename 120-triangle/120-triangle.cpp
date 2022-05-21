e6ca191206d08bfffeea85564a8cfbcbdd0f83c7
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-21 15:01:17 +0530

    Time: 4 ms (90.28%), Space: 9.1 MB (10.23%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -24,7 +24,24 @@ public:
         int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         
-         return recurse(triangle,dp,0,0,n);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==n-1)
+                dp[n-1][j]=triangle[i][j];
+                else
+                {
+                     
+                  int down=triangle[i][j]+dp[i+1][j];
+                  int down_diag=triangle[i][j]+dp[i+1][j+1];
+                  dp[i][j]=min(down,down_diag);
+                }
+                    
+                    
+            }
+        }
+         return dp[0][0];
      
      
 
