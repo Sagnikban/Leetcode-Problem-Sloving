b24a18c0e18c6d83eb403e4518cd21a755128b6c
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-21 14:57:06 +0530

    Time: 13 ms (19.50%), Space: 9 MB (25.20%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
- int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle,int n, 
-vector<vector<int> > &dp) {
-  
-  if(dp[i][j]!=-1)
-  return dp[i][j];
-  
-  if(i==n-1) return triangle[i][j];
-    
-  int down = triangle[i][j]+minimumPathSumUtil(i+1,j,triangle,n,dp);
-  int diagonal = triangle[i][j]+minimumPathSumUtil(i+1,j+1,triangle,n,dp);
-  
-  return dp[i][j] = min(down, diagonal);
-  
-}
+    int recurse(vector<vector<int>> &triangle,vector<vector<int>> &dp,int i,int j,int n)
+    {
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        
+       if(i==n-1)
+       {
+           return triangle[i][j];
+       }
+        
+   
+        
+        int down=triangle[i][j]+recurse(triangle,dp,i+1,j,n);
+        int down_diag=triangle[i][j]+recurse(triangle,dp,i+1,j+1,n);
+        
+        return dp[i][j]=min(down,down_diag);
+        
+        
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
+        
         int n=triangle.size();
-     vector<vector<int> > dp(n,vector<int>(n,-1));
-    return minimumPathSumUtil(0,0,triangle,n,dp);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+         return recurse(triangle,dp,0,0,n);
+     
+     
 
     }
 };
