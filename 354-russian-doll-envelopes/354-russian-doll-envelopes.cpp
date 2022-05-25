2ccf07dbdee3a849375dcc3d72b154b22353ece7
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-05-25 14:04:24 +0530

    Time: 439 ms (66.75%), Space: 77.6 MB (70.83%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    
     static bool cmp(vector<int>&a,vector<int>&b)
-
 {
-   if(a[0]==b[0]) return a[1] > b[1];
+   if(a[0]==b[0]) 
+    return a[1] > b[1];
     return a[0] < b[0];
 }
 
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-
-          int n=envelopes.size();
+    
+    int n=envelopes.size();
    
     vector<int>dp;
  
