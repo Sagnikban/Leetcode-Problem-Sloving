6b2fbd8d610c752fc6ab17da7df04f159e133de2
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-01 22:17:52 +0530

    Time: 40 ms (75.85%), Space: 12.3 MB (59.40%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -12,16 +12,16 @@ public:
     {
        grid[i][j]='0';
        
-       if( isvalid( i-1, j, n,m,grid))
+       if( i-1>=0 && i-1<n && j>=0 && j<m && grid[i-1][j]=='1')
        dfs(i-1,j,n,m,grid);
        
-       if(isvalid(i+1, j,n,m,grid))
+       if(i+1>=0 && i+1<n && j>=0 && j<m && grid[i+1][j]=='1')
        dfs(i+1, j,n,m,grid);     
             
-      if(isvalid( i,j+1,n, m,grid))
+      if(i>=0 && i<n && j+1>=0 && j+1<m && grid[i][j+1]=='1')
       dfs(i,j+1,n,m,grid);
        
-      if(isvalid( i, j-1,n, m,grid))
+      if(i>=0 && i<n && j-1>=0 && j-1<m && grid[i][j-1]=='1')
       dfs(i,j-1,n, m,grid);   
     }
     
