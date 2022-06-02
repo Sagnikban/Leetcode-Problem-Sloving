031ad717ddb1f3ae466f79a00eefb9bbd7634a27
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-02 13:14:59 +0530

    Time: 27 ms (63.41%), Space: 14.1 MB (46.87%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool DFSRec(int s,vector<bool> &visited,vector<bool> &currvisited,vector<int>adj[])
+    {
+        visited[s]=true;
+        currvisited[s]=true;
+        
+        vector<int>data=adj[s];
+        for(auto it:data)
+        {
+            if(!visited[it])
+            {
+                if(DFSRec(it,visited,currvisited,adj))
+                return true;    
+            }
+            else if(visited[it]&& currvisited[it])
+            return true;    
+        }
+        
+        
+        currvisited[s]=false;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>adj[numCourses];
+        
+        for(int i=0;i<prerequisites.size();i++)
+        adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            
+        
+        
+       vector<bool>visited(numCourses,false),currvisited(numCourses,false);
+        
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!visited[i])
+            {
+                if(DFSRec(i,visited,currvisited,adj))
+                return false;
+            }
+        }
+         
+        return true;
+        
+    }
+};
