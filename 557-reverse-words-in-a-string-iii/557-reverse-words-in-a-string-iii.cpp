40b171bfcbb5f1163bec0ad6df7305b1e2bc41b0
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-02 13:19:42 +0530

    Time: 11 ms (93.40%), Space: 13.5 MB (19.27%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        string rev="";
+        string ans="";
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==' ')
+            {
+                reverse(rev.begin(),rev.end());
+                ans+=rev+' ';
+                rev="";
+                
+            }
+            else
+            {
+                rev+=s[i];
+            }
+            
+            
+        }
+        reverse(rev.begin(),rev.end());
+                ans+=rev;
+        
+        return ans;
+        
+        
+    }
+};
