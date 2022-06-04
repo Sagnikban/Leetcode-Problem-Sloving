c1ea16273e7ff1878948ce6c9c66c71043831029
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-04 14:18:48 +0530

    Time: 17 ms (58.95%), Space: 9 MB (56.26%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -22,7 +22,24 @@ public:
       
         int m=word1.size();
         int n=word2.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return recurse(m-1,n-1,word1,word2,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++)
+        dp[i][0]=i;
+        
+        for(int i=1;i<=n;i++)
+        dp[0][i]=i;
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                dp[i][j]=dp[i-1][j-1];
+                else
+                dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});    
+            }
+        }
+        
+        return dp[m][n];
     }
 };
