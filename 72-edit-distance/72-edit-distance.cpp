8123b41103f47f16c15c967e9d9ae6f0413f6bd0
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-04 14:30:47 +0530

    Time: 14 ms (70.38%), Space: 6.5 MB (94.77%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    //Recursion and Memorisation
-    int recurse(int i,int j,string word1,string word2, vector<vector<int>>&dp)
-    {
-        if(i<0)
-         return j+1;     
-            
-        if(j<0)
-        return i+1;
-        
-        
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        
-        if(word1[i]!=word2[j])
-        return dp[i][j]=1+min({recurse(i-1,j-1,word1,word2,dp),recurse(i-1,j,word1,word2,dp),recurse(i,j-1,word1,word2,dp)});
-        
-        return dp[i][j]=recurse(i-1,j-1,word1,word2,dp);
-        
+    
+    //Tabulation Approach+Space Optimized
+    int minDistance(string S1, string S2) {
+       int n = S1.size();
+    int m = S2.size();
+    
+    vector<int> prev(m+1,0);
+    vector<int> cur(m+1,0);
+    
+    for(int j=0;j<=m;j++){
+        prev[j] = j;
     }
     
-    //Tabulation Approach
-    int minDistance(string word1, string word2) {
-      
-        int m=word1.size();
-        int n=word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        for(int i=0;i<=m;i++)
-        dp[i][0]=i;
-        
-        for(int i=1;i<=n;i++)
-        dp[0][i]=i;
-        
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(word1[i-1]==word2[j-1])
-                dp[i][j]=dp[i-1][j-1];
-                else
-                dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});    
-            }
+    for(int i=1;i<n+1;i++){
+        cur[0]=i;
+        for(int j=1;j<m+1;j++){
+            if(S1[i-1]==S2[j-1])
+                cur[j] = 0+prev[j-1];
+            
+            else cur[j] = 1+min(prev[j-1],min(prev[j],cur[j-1]));
         }
-        
-        return dp[m][n];
+        prev = cur;
+    }
+    
+    return prev[m];
     }
 };
