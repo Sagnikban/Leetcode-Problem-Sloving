b026f164c1e65177516d00c4a90e2ea8978b7e89
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-07 12:47:28 +0530

    Time: 7 ms (24.43%), Space: 9.2 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-         int i=m-1;
+        int i=m-1;
         int j=n-1;
          int k=m+n-1;
         
@@ -33,7 +32,7 @@ public:
             nums1[k--]=nums2[j--];
         }
         
+       
         
     }
-    
 };
