9591896db1c1ed4735df37717ae461db84efd396
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-08 21:26:20 +0530

    Time: 7 ms (7.40%), Space: 6.3 MB (26.79%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        if(rev==s)
+        return 1;
+        
+        return 2;
+            
+    }
+};
