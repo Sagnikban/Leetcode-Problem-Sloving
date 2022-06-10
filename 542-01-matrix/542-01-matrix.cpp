ac10f641b7ec74a0b4652181930ed68e4059dda9
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-10 13:32:24 +0530

    Time: 91 ms (73.80%), Space: 30.3 MB (51.27%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -3,14 +3,13 @@ class Solution {
    public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         queue<pair<int,int>>q;
-        int rows = mat.size();
-         if (rows == 0)
-        return mat;
+        int m=mat.size();
+        int n=mat[0].size();
         vector<vector<int>> dist(mat.size(), vector<int> (mat[0].size(), INT_MAX));
         
-        for(int i=0;i<mat.size();i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<mat[0].size();j++)
+            for(int j=0;j<n;j++)
             {
                 if(mat[i][j]==0)
                 {
@@ -31,7 +30,7 @@ class Solution {
                 int x=p.first+dir[i][0];
                 int y=p.second+dir[i][1];
                 
-                if(x>=0 && x<mat.size() && y>=0 &&  y<mat[0].size())
+                if(x>=0 && x<m&& y>=0 &&  y<n)
                 {
                       if (dist[x][y] > dist[p.first][p.second] + 1) {
                         dist[x][y] = dist[p.first][p.second] + 1;
