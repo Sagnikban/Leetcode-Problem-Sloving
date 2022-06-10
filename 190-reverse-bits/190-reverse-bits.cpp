9e3171be7dcb246f1a5babf313b5efd64b77b7ca
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-10 10:22:54 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.37%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-            if (n == 0) return 0;
+            
     
         int result = 0;
         for (int i = 0; i < 32; i++) {
