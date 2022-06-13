52de2432d26c75a09e396907583db85a35fcdd25
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-13 22:52:41 +0530

    Time: 289 ms (13.31%), Space: 50.3 MB (62.09%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void dfs(int src,vector<int>g[],vector<int>&vis)
+    void dfs(int src,vector<int>g[],vector<bool>&vis)
 {
-    vis[src]=1;
+    vis[src]=true;
     for(auto it:g[src])
     {
         if(!vis[it])
@@ -14,22 +14,22 @@ public:
 
 int makeConnected(int n, vector<vector<int>>& connections) 
 {
-    vector<int>g[n];
+    vector<int>adj[n];
     int m=connections.size();
     if(m<n-1)return -1;
     for(int i=0;i<m;i++)
     {
-        g[connections[i][1]].push_back(connections[i][0]);
-        g[connections[i][0]].push_back(connections[i][1]);
+        adj[connections[i][1]].push_back(connections[i][0]);
+        adj[connections[i][0]].push_back(connections[i][1]);
     }
-    vector<int>vis(n,0);
+    vector<bool>vis(n,false);
     int c=0;
     for(int i=0;i<n;i++)
     {
         if(!vis[i])
         {
             c++;
-            dfs(i,g,vis);
+            dfs(i,adj,vis);
         }
     }
     return c-1;
