627acc9c55df43bec5e13dc451d02211127b7e0f
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-14 09:16:12 +0530

    Time: 10 ms (83.96%), Space: 11.6 MB (49.37%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peak_find(vector<int>& arr, int low, int high)
+    {
+        if(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid] > arr[mid-1]  &&  arr[mid] > arr[mid+1])
+                return mid;
+            else if(arr[mid] > arr[mid-1]  &&  arr[mid] < arr[mid+1])
+                return peak_find(arr, mid+1, high);
+            else 
+                return peak_find(arr, low, mid);
+        }
+        return 10;
+    }
+    
+    int peakIndexInMountainArray(vector<int>& A) {
+        int sz = A.size();
+        int k = peak_find(A, 0, sz-1);
+        return k;
+    }
+};
