731c480f48bc41d6a78a0be3ee69fde9c162b248
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-14 09:19:37 +0530

    Time: 3 ms (92.92%), Space: 9.7 MB (72.61%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -8,28 +8,7 @@ public:
         if(target<nums[0])
         return 0;
         
-        
-        int l=0,r=nums.size()-1;
-        int flag=2;
-        while(l<=r)
-        {
-             int mid=(l+r)/2;
-            
-             if(nums[mid]==target)
-             {
-               flag=1;
-               return mid;
-             }
-            if(nums[mid]<target)
-            l=mid+1;
-            
-            if(nums[mid]>target)
-            r= mid-1;
-             
-      }
-        vector<int>::iterator it;
-        
-
+     
         return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
         
         return 0;
