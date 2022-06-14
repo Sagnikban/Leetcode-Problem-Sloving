4b540b94474cd1d4f1cbf76f9735ae5ba1e749db
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-14 09:16:40 +0530

    Time: 11 ms (77.11%), Space: 11.6 MB (49.37%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,11 +4,13 @@ public:
     {
         if(low<=high)
         {
-            int mid = (low+high)/2;
+            int mid = low+(high-low)/2;
             if(arr[mid] > arr[mid-1]  &&  arr[mid] > arr[mid+1])
                 return mid;
+            
             else if(arr[mid] > arr[mid-1]  &&  arr[mid] < arr[mid+1])
                 return peak_find(arr, mid+1, high);
+            
             else 
                 return peak_find(arr, low, mid);
         }
