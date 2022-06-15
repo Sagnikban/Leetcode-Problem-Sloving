de3057aa3606570b1ca251790fda65b98daa5292
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-15 08:54:21 +0530

    Time: 32 ms (57.55%), Space: 19.2 MB (83.42%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-          int n = nums.size();
-
-    // Count the zeroes
-    int numZeroes = 0;
-    for (int i = 0; i < n; i++) {
-        numZeroes += (nums[i] == 0);
-    }
-
-    // Make all the non-zero elements retain their original order.
-    vector<int> ans;
-    for (int i = 0; i < n; i++) {
+        int lastNonZeroFoundAt = 0;
+    // If the current element is not 0, then we need to
+    // append it just in front of last non 0 element we found. 
+    for (int i = 0; i < nums.size(); i++) {
         if (nums[i] != 0) {
-            ans.push_back(nums[i]);
+            nums[lastNonZeroFoundAt++] = nums[i];
         }
     }
-
-    // Move all zeroes to the end
-    while (numZeroes--) {
-        ans.push_back(0);
-    }
-
-    // Combine the result
-    for (int i = 0; i < n; i++) {
-        nums[i] = ans[i];
+ 	// After we have finished processing new elements,
+ 	// all the non-zero elements are already at beginning of array.
+ 	// We just need to fill remaining array with 0's.
+    for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+        nums[i] = 0;
     }
     }
 };
