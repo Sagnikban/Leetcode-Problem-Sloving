32ad7b4d677608815ea43404064f46d7c0a99fb5
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-15 22:46:01 +0530

    Time: 579 ms (55.52%), Space: 102.8 MB (83.17%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(int node,vector<pair<int,bool>> adj[],vector<bool> &vis,int &count){
+        vis[node] = true;
+        for(auto i:adj[node]){
+            if(!vis[i.first]){
+                if(i.second){
+                    count++;
+                }
+                dfs(i.first,adj,vis,count);
+            }
+        }
+    }
+    
+    int minReorder(int n, vector<vector<int>>& c) {
+        vector<pair<int,bool>> adj[n];
+        for(int i=0;i<c.size();i++){
+            adj[c[i][0]].push_back({c[i][1],true});
+            adj[c[i][1]].push_back({c[i][0],false});
+        }
+        
+        int count=0;
+        vector<bool> vis(n,false);
+        dfs(0,adj,vis,count);
+        
+        return count;
+    }
+};
