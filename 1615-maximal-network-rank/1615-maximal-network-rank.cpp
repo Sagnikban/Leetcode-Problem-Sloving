641fcc31442534f2ba46b7fd60339ac6f716617e
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-19 12:27:44 +0530

    Time: 172 ms (28.94%), Space: 42.3 MB (11.48%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int>adj[n];
-        for(int i=0;i<roads.size();i++)
-        {
-            adj[roads[i][0]].push_back(roads[i][1]);
-            adj[roads[i][1]].push_back(roads[i][0]);
-        }
-        
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-               int x=adj[i].size()+adj[j].size();
-               sort(adj[i].begin(),adj[i].end());
-               if(binary_search(adj[i].begin(),adj[i].end(),j))
-               x--;
-        
-               maxi=max(maxi,x); 
-                
+    vector<set<int>> network(n);
+		for (auto i : roads)
+		{
+			network[i[0]].insert(i[1]);
+			network[i[1]].insert(i[0]);
+		}
 
-            }
-        }
-        return maxi;
+		int mostRoads = 0;
+		for (int i = 0; i < n; ++i)
+			for (int j = i + 1; j < n; ++j)
+			{
+				int curRoads = network[i].size() + network[j].size();
+				if (network[i].find(j) != network[i].end()) --curRoads;
+				mostRoads = max(mostRoads, curRoads);
+			}
+
+		return mostRoads;
     }
 };
