c0950814f1003df6170f7e1c5b9c0ff5d2d0a254
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-22 23:17:21 +0530

    Time: 883 ms (15.62%), Space: 167.8 MB (84.22%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        map<string,int>same_letter;
+        
+            int ans=0;
+        
+        for(int i=0;i<words.size();i++)
+        {
+            if(words[i][0]==words[i][1])
+            {
+            same_letter[words[i]]++;    
+            }
+        } 
+        
+        for(auto it:same_letter)
+        {
+            int x=it.second/2;
+            
+            ans+=x*4;    
+            
+            it.second-=x*2;
+        }
+        
+        for(auto it:same_letter)
+        {
+            if(it.second%2==1)
+            {
+                ans+=2;
+                break;
+            }    
+                
+        }
+       cout<<ans<<" ";
+        map<string,int>p;
+    
+        for(int i=0;i<words.size();i++)
+        {
+            if(words[i][0]==words[i][1])
+            continue;
+            
+            reverse(words[i].begin(),words[i].end());
+            if(p.find(words[i])!=p.end() && p[words[i]]!=0)
+            {
+                auto it=p.find(words[i]);
+                p[words[i]]-=1;
+                ans+=4;
+            }    
+             else
+             {
+                 
+             reverse(words[i].begin(),words[i].end());
+             p[words[i]]++;
+             }
+
+            
+        }
+        return ans;
+    }
+};
