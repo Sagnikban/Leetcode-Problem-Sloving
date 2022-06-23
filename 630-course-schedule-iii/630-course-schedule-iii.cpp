e976eb04cef9a44741cac25383d837b499ab7170
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-23 20:36:03 +0530

    Time: 448 ms (29.18%), Space: 56.1 MB (97.05%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -3,10 +3,8 @@ public:
      //comparartor function to comare if the lastday of a course
     static bool comp(vector<int> &a, vector<int> &b){
         return a[1] < b[1];
-    }
-    
+    }    
     int scheduleCourse(vector<vector<int>> &courses){
-        
         //soring according to lastday in increasing order.
         sort(courses.begin(), courses.end(), comp);
         
@@ -14,19 +12,19 @@ public:
         priority_queue<int> maxD;
 
         int time = 0; // time to store the total time taken
-        for (auto course : courses){
+        for (int i=0;i<courses.size();i++){
             
             //if total time taken including this course is lesser than or equak to the last day then add couse to queue and add the time to total time
-            if (time + course[0] <= course[1]){
-                time += course[0];
-                maxD.push(course[0]);
+            if (time + courses[i][0] <= courses[i][1]){
+                time += courses[i][0];
+                maxD.push(courses[i][0]);
             }
             
             //else if there are element in priority queue then check if current course is shorter than the max previous couse then just replace that with current
-            else if (!maxD.empty() && maxD.top() >= course[0]){
-                time = time - maxD.top() + course[0]; //replacing max previous course with current in toytal time
+            else if (!maxD.empty() && maxD.top() >= courses[i][0]){
+                time = time - maxD.top() + courses[i][0]; //replacing max previous course with current in toytal time
                 maxD.pop();                           //taking out max previous
-                maxD.push(course[0]);                 //adding current course
+                maxD.push(courses[i][0]);                 //adding current course
             }
         }
         
