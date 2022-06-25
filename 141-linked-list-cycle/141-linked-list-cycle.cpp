aaad437cab2d20625f177de79068e35c736b612d
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-25 09:53:49 +0530

    Time: 7 ms (97.37%), Space: 8.1 MB (57.12%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -13,11 +13,13 @@ public:
 ListNode* fast = head;
 
 while ( fast && fast->next) {
+   
     slow = slow->next;          
     fast = fast->next->next;   
-    if (slow == fast) {         
+      if (slow == fast) {         
         return true;
     }
+   
 }
 return false;  
     }
