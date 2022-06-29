056f1f8a8ae7cf5b21ba2e4030664cfbbb65b8a5
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-29 12:23:29 +0530

    Time: 130 ms (92.48%), Space: 29.9 MB (65.01%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -7,16 +7,16 @@ public:
     
     int nearestExit(vector<vector<char>>& grid, vector<int>& entrance) {
         int ans = 0;
-        queue<pair<int,int>>qu;
-        qu.push({entrance[0], entrance[1]});
+        queue<pair<int,int>>q;
+        q.push({entrance[0], entrance[1]});
         
-        while(!qu.empty()){
-            int size = qu.size();
+        while(!q.empty()){
+            int size = q.size();
             
             while(size--){
-                int i = qu.front().first;
-                int j = qu.front().second;
-                qu.pop();
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
                 
                 if((i == 0 or j == 0 or i==grid.size()-1 or j == grid[0].size()-1) and (i!=entrance[0] or j!=entrance[1])){
                     return ans;
@@ -28,7 +28,7 @@ public:
 
                     if(currx>=0 and currx<grid.size() and curry>=0 and curry<grid[0].size() and grid[currx][curry]=='.'){
                         grid[currx][curry] = '+';       
-                        qu.push({currx, curry});
+                        q.push({currx, curry});
                     }
                 }
                 
