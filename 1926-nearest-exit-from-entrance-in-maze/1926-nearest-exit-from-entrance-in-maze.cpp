a0727a060a9d4906ff1a09d00fcd63ee7b93bf42
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-29 12:22:41 +0530

    Time: 211 ms (40.73%), Space: 29.9 MB (80.70%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,60 +1,44 @@
-struct node{
-    int x;
-    int y;
-    int c;
-    node(int x,int y,int c)
-    {
-        this->x=x;
-        this->y=y;
-        this->c=c;
-    }
-};
 class Solution {
 public:
-    bool check(int i,int j,int n,int m,vector<int> &e)
-    {
-        if(i==e[0]&&j==e[1])
-        {
-            return false;
+  //up down left right
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    
+    
+    int nearestExit(vector<vector<char>>& grid, vector<int>& entrance) {
+        int ans = 0;
+        queue<pair<int,int>>qu;
+        qu.push({entrance[0], entrance[1]});
+        
+        while(!qu.empty()){
+            int size = qu.size();
+            
+            while(size--){
+                int i = qu.front().first;
+                int j = qu.front().second;
+                qu.pop();
+                
+                if((i == 0 or j == 0 or i==grid.size()-1 or j == grid[0].size()-1) and (i!=entrance[0] or j!=entrance[1])){
+                    return ans;
+                }
+                
+                for(int d = 0; d<4; d++){
+                    int currx = i+dx[d];
+                    int curry = j+dy[d];
+
+                    if(currx>=0 and currx<grid.size() and curry>=0 and curry<grid[0].size() and grid[currx][curry]=='.'){
+                        grid[currx][curry] = '+';       
+                        qu.push({currx, curry});
+                    }
+                }
+                
+                
+            }
+            
+            ans++;
         }
-        if(i==0||j==0||i==n-1||j==m-1)
-        return true;
-        return false;
-    }
-    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        node n1(entrance[0],entrance[1],0);
-        int n=maze.size();
-        int m=maze[0].size();
-        vector<vector<int>> dir;
-        dir.push_back({0,1});
-        dir.push_back({0,-1});
-        dir.push_back({-1,0});
-        dir.push_back({1,0});
-        queue<node> q;
-        q.push(n1);
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        while(!q.empty())
-        {
-            node ele=q.front();
-            q.pop();
-            int i=ele.x;
-            int j=ele.y;
-            int c=ele.c;
-            if(check(i,j,n,m,entrance))
-            {
-                return c;
-            }
-            for(int a=0;a<=3;a++)
-            {
-                        int new_x=i+dir[a][0];
-                        int new_y=j+dir[a][1];
-                        if(new_x>=0&&new_x<n&&new_y>=0&&new_y<m&&maze[new_x][new_y]=='.'&&vis[new_x][new_y]==false)
-                        {
-                            vis[new_x][new_y]=true;
-                            q.push(node(new_x,new_y,c+1));
-                        }       
-            }
-            }
+        
         return -1;
     }
+
 };
