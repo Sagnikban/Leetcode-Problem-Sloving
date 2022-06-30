78932101d7913f9cae1d8a83310b9d6cfbf74725
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-06-30 10:21:15 +0530

    Time: 25 ms (14.20%), Space: 11 MB (5.28%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,23 +1,25 @@
 #define ll long long int
 class Solution {
 public:
-    int minMoves2(vector<int>& nums) {
+    int minMoves2(vector<int>& nums) 
+    {
+        ll n=nums.size();
         sort(nums.begin(),nums.end());
         ll prefix[nums.size()];
         ll suffix[nums.size()];
         prefix[0]=nums[0];
-        suffix[nums.size()-1]=nums[nums.size()-1];
-        for(ll i=1;i<nums.size();i++)
+        suffix[n-1]=nums[n-1];
+        for(ll i=1;i<n;i++)
         prefix[i]=prefix[i-1]+nums[i];
         
-        for(ll j=nums.size()-2;j>=0;j--)
+        for(ll j=n-2;j>=0;j--)
         suffix[j]=suffix[j+1]+nums[j];
         
         ll  mini=INT_MAX;
         
-        for(ll i=0;i<nums.size();i++)
+        for(ll i=0;i<n;i++)
         {
-            ll s=suffix[i]-(nums.size()-i)*nums[i];
+            ll s=suffix[i]-(n-i)*nums[i];
             ll p=(i+1)*nums[i]-prefix[i];
             //cout<<s<<" "<<p<<endl;
             mini=min(mini,s+p);
