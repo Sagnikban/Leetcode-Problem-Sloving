455c7ca049fa2727fbdf67b78c911e1b42fcf0f9
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-01 11:32:51 +0530

    Time: 136 ms (30.38%), Space: 16.6 MB (47.05%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool sortbys(const pair<int,int> &a,
+              const pair<int,int> &b)
+{
+    return (a.second>  b.second);
+}
+  
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<boxTypes.size();i++)
+        v.push_back({boxTypes[i][0],boxTypes[i][1]});
+        sort(v.begin(),v.end(),sortbys);
+        int c=0,ans=0;
+        for(int i=0;i<v.size();i++)
+        {
+            
+            if(truckSize==0)
+            break;
+            
+            int x=min(truckSize,v[i].first);
+            truckSize-=x;
+            
+            ans+=v[i].second*x;
+            cout<<x<<" "<<ans<<" "<<truckSize<<endl;
+        }
+        return ans;
+    }
+};
