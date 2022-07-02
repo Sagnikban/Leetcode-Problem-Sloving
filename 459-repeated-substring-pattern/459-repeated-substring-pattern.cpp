6790bcaccd04357aa00df2c0ce1c43e97c83e282
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-02 13:23:08 +0530

    Time: 143 ms (17.78%), Space: 91.5 MB (16.60%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string st="";
+        for(int i=0;i<s.length()-1;i++)
+        {
+            st+=s[i];
+            if(s.length()%st.length()!=0)
+            continue;
+            
+            else
+            {
+                string temp="";
+                int l=s.length()/st.length();
+                for(int i=0;i<l;i++)
+                temp+=st;
+                
+                if(temp==s)
+                {
+                    cout<<st<<endl;
+                    return true; 
+                }
+                   
+            }
+        }
+        return false;
+    }
+};
