4ea210d80e186c3ad975f350566cb6d6d6f62c5e
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-02 13:51:29 +0530

    Time: 15 ms (60.46%), Space: 12.1 MB (20.39%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -17,7 +17,31 @@ bool isoperator(string c){
 }
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        int i,val1,val2; stack<string> op; stack<int> num; for(i=0;i<tokens.size();i++){ if(isoperator(tokens[i])){ op.push(tokens[i]); }else { num.push(getNumber(tokens[i])); } if(num.size()>=2 && op.size()>0) { val1 = num.top(); num.pop(); val2 = num.top(); num.pop(); if(op.top()=="+"){ val2 = val1+val2; } else if(op.top()=="-"){ val2 = val2-val1; } else if(op.top()=="*"){ val2 = val2*val1; } else { val2 = val2/val1; } num.push(val2); op.pop(); } } return num.top();
-    }
+    int evalRPN(vector<string>& tokens)
+    {
+        int i,val1,val2; 
+        stack<string> op; stack<int> num;
+        for(i=0;i<tokens.size();i++)
+        { 
+        if(isoperator(tokens[i]))
+         op.push(tokens[i]); 
+        else
+         num.push(getNumber(tokens[i]));  
+            if(num.size()>=2 && op.size()>0) 
+            { val1 = num.top();
+             num.pop(); 
+             val2 = num.top();
+             num.pop();
+             if(op.top()=="+")
+             val2 = val1+val2; 
+             else if(op.top()=="-")
+             val2 = val2-val1;  
+             else if(op.top()=="*")
+             val2 = val2*val1; 
+             else 
+             val2 = val2/val1; 
+             num.push(val2);
+             op.pop(); } }
+             return num.top();
+      }
 };
