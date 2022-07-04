bbbf95e311cdc159819bc903ca8886d986cedd92
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-04 23:13:24 +0530

    Time: 49 ms (18.72%), Space: 17.2 MB (96.69%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum = 0;
+        int left2right[ratings.size()];
+        int right2left[ratings.size()];
+        
+        for(int i=0;i<ratings.size();i++)
+        left2right[i]=1,right2left[i]=1;    
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left2right[i] = left2right[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.size()- 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                right2left[i] = right2left[i + 1] + 1;
+            }
+        }
+        for (int i = 0; i < ratings.size(); i++) {
+            sum += max(left2right[i], right2left[i]);
+        }
+        return sum;
+    }
+};
