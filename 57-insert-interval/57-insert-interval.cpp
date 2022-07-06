6cd616d9c3f298da03dca77871bcd9242e85ea68
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-06 14:07:21 +0530

    Time: 29 ms (37.98%), Space: 17.1 MB (74.42%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
 
     vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& ne) 
-    {   vector<vector<int>>ans;int n=v.size();int i;
+        
+    {   
+        vector<vector<int>>ans;int n=v.size();int i;
        //Push all the intervals whose starting value is less than our new interval 
+       
         for(i=0;i<n;i++){
             if(v[i][0]<ne[0])
             ans.push_back(v[i]);
             else
             break;
         }
+        
 		//check if we can merge our new interval with the previous one
         if(!ans.empty() && ans.back()[1]>=ne[0]){
             ans.back()[1]=max(ans.back()[1],ne[1]);
@@ -17,12 +21,15 @@ public:
         else{
             ans.push_back(ne);
         }
-        for(int j=i;j<n;j++){
+        while(i<n)
+        {
 		//From now after inserting new interval there is chance of merging so keep checking
-            if(!ans.empty() && ans.back()[1]>=v[j][0])
-            ans.back()[1]=max(ans.back()[1],v[j][1]);
+            if(!ans.empty() && ans.back()[1]>=v[i][0])
+            ans.back()[1]=max(ans.back()[1],v[i][1]);
             else
-            ans.push_back(v[j]);
+            ans.push_back(v[i]);
+            
+            i++;
         }
         return ans;
     }
