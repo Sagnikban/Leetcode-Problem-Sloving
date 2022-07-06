08442572d85380a235cb4289345279454a0cbb21
Sagnik Banerjee <81607521+Sagnikban@users.noreply.github.com>
2022-07-06 14:10:20 +0530

    Time: 18 ms (79.53%), Space: 17.1 MB (38.09%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -4,14 +4,16 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& ne) 
         
     {   
-        vector<vector<int>>ans;int n=v.size();int i;
+        vector<vector<int>>ans;int n=v.size();
        //Push all the intervals whose starting value is less than our new interval 
-       
-        for(i=0;i<n;i++){
+       int i=0;
+        while(i<n)
+        {
             if(v[i][0]<ne[0])
             ans.push_back(v[i]);
             else
             break;
+            i++;
         }
         
 		//check if we can merge our new interval with the previous one
